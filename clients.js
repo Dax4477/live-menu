a54b65f006110d48f841b1c5ad6398fdd4cd16d1
Dax4477 <156528907+Dax4477@users.noreply.github.com>
2026-02-14 23:01:28 +0530

    Update clients.js

--- clients.js
+++ clients.js
@@ -14,7 +14,7 @@ const OBSERA_CLIENTS = {
         tag: "24h Active",
         hours: 24,
         mapLink: "https://maps.google.com/?q=Kozhikode+Medical+College",
-        menuSheetId: "PUT_MEDICAL_MENU_SHEET_ID_HERE",
+        menuSheetId: "1s9NCrlKf3i4lt4VL3WKp8A9o9DXYrsE3ccddPVkMyk8",
         adSheetId: "PUT_MEDICAL_AD_SHEET_ID_HERE"
     }
 };

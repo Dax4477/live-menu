7ae43bcfb090b802eb3f8ac64688d8bedaab23ff
Dax4477 <156528907+Dax4477@users.noreply.github.com>
2026-02-16 16:52:05 +0530

    Update clients.js

--- clients.js
+++ clients.js
@@ -25,6 +25,14 @@ const OBSERA_CLIENTS = {
         menuSheetId: "1Q5yinqDrV4zsHMLQBm7ieHPlVLMAUK9vdAFOaMLQ73c",
         adSheetId: "1OhzIcctYTyeKQEA-9K5tFZnQqeEu38r-dHQfvXdby3s"
     },
+     "test2": {
+        name: "coffeday",
+        tag: "10h Active",
+        hours: 10,
+        mapLink: "",
+        menuSheetId: "1Q5yinqDrV4zsHMLQBm7ieHPlVLMAUK9vdAFOaMLQ73c",
+        adSheetId: "1OhzIcctYTyeKQEA-9K5tFZnQqeEu38r-dHQfvXdby3s"
+    },
 };
  
 
